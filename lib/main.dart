699ac1ecec5bba16e806ca5bc2022a226dd42e79
shubhu1026 <shubhu.p2000@gmail.com>
2024-01-26 21:51:25 -0500

    Added text fields and calc button

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,81 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Paycheck Calculator',
-      home: Scaffold(
-        body: Text('Hello'),
+      home: PaycheckCalculator(),
+    );
+  }
+}
+
+class PaycheckCalculator extends StatefulWidget {
+  const PaycheckCalculator({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _PaycheckCalculatorState();
+}
+
+class _PaycheckCalculatorState extends State<PaycheckCalculator> {
+  TextEditingController hoursController = TextEditingController();
+  TextEditingController rateController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Paycheck Calculator'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                TextField(
+                  controller: hoursController,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    hintText: 'Enter Hours Worked',
+                  ),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                TextField(
+                  controller: rateController,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    hintText: 'Enter Hourly Rate',
+                  ),
+                ),
+                const SizedBox(
+                  height: 32,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      // Calculate paycheck
+                      double? hours = double.tryParse(hoursController.text);
+                      double? rate = double.tryParse(rateController.text);
+                    });
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                  ),
+                  child: const Text(
+                    'Calculate',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
